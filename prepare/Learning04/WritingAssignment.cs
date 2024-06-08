ee1e1e3ced090011452973d80e107eda69d982cf
John <johnbrown@byui.edu>
2024-06-07 19:21:12 -0600

    finished prepare 4

---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,22 @@
+namespace Assign
+{
+    4public class WritingAssignment : Assignment
+{
+    private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        // Here we set any variables specific to the WritingAssignment class
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        // Notice that we are calling the getter here because _studentName is private in the base class
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+}
+}
